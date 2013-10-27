ce3c590d2ef13c236292e769e451db213477bb3a
Owen O <owen.obrien@hotmail.co.uk>
2013-10-27 22:48:53 +0000

    Created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findfriends::Application.config.secret_token = 'cd2a64f5216614342033d026a6e0f6aa9f31b52c973a8d495086667bb38e22cc992e84053a74b598bb63cc4894b06fa43df55caa63e654250e3b461b1f52f161'
